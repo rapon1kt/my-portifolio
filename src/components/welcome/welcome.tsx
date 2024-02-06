f57fef0a69ddb167f33328f5fef517c7322692a9
rapon1kt <shinnatensei@gmail.com>
2024-02-06 20:50:47 -0300

    feat: create welcome component

---
+++ src/components/welcome/welcome.tsx
@@ -0,0 +1,81 @@
+"use client";
+import Social from "./social-component/social-component";
+import socialMedia from "@/data/social";
+import { useEffect } from "react";
+import "./welcome.css";
+
+export default function Welcome() {
+	const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	let interval: any;
+
+	useEffect(() => {
+		document.querySelector("h1")!.onmouseover = (event: any) => {
+			let iteration = 0;
+			let i = 0;
+			const txt = " my portifolio!";
+			const speed = 50;
+			const chipText = document.getElementById("chip-text");
+
+			clearInterval(interval);
+			function typeWriter() {
+				if (chipText) {
+					if (chipText.textContent!.length === 25) {
+						chipText.innerHTML += "";
+					} else if (i < txt.length) {
+						chipText.innerHTML += txt.charAt(i);
+						i++;
+						setTimeout(typeWriter, speed);
+					}
+				} else {
+					return;
+				}
+			}
+			typeWriter();
+
+			interval = setInterval(() => {
+				event.target!.innerText = event.target.innerText
+					.split("")
+					.map((letter: string, index: number) => {
+						if (index < iteration) {
+							return event.target.dataset.value[index];
+						}
+
+						return letters[Math.floor(Math.random() * 26)];
+					})
+					.join("");
+
+				if (iteration >= event.target.dataset.value.length) {
+					clearInterval(interval);
+				}
+
+				iteration += 1 / 3;
+			}, 30);
+		};
+	}, []);
+
+	return (
+		<main id="Welcome">
+			<div className="left-corner">
+				<div className="chip">
+					<p id="chip-text">Welcome to</p>
+				</div>
+				<h1 data-value="Gustavo Raponi">My Portifolio!</h1>
+				<p>16y, Full-Stack Developer</p>
+				<div className="social">
+					{socialMedia.map((media) => (
+						<Social Icon={media.icon} link={media.link} />
+					))}
+				</div>
+			</div>
+			<div className="middle-corner">
+				<div id="blob"></div>
+				<div id="blur"></div>
+				<div id="picture"></div>
+			</div>
+			<div className="right-corner">
+				<button className="email-button">Send me an email!</button>
+			</div>
+		</main>
+	);
+}
