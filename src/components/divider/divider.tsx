9499a9cf380461686144c8dc136d83bd3f2c4d57
rapon1kt <shinnatensei@gmail.com>
2024-02-06 20:51:09 -0300

    feat: create divider component

---
+++ src/components/divider/divider.tsx
@@ -0,0 +1,10 @@
+import "./divider.css";
+
+export default function Divider() {
+	return (
+		<div id="divider-container">
+			<hr className="divider" />
+			<hr className="divider" />
+		</div>
+	);
+}
