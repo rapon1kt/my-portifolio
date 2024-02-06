437ab314e565940926abb90fea15c343beec431f
rapon1kt <shinnatensei@gmail.com>
2024-02-06 19:59:39 -0300

    feat: create navbar component and navbar styles

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,29 @@
+import { Rocket } from "lucide-react";
+import "./navbar.css";
+
+export default function Navbar() {
+	return (
+		<nav id="navbar">
+			<div className="logo">
+				<Rocket />
+			</div>
+			<ul className="nav-links">
+				<li className="nav-link">
+					<a href="#">Home</a>
+				</li>
+				<li className="nav-link">
+					<a href="#about-me">About Me</a>
+				</li>
+				<li className="nav-link">
+					<a href="#skills">Skills</a>
+				</li>
+				<li className="nav-link">
+					<a href="#projects">Projects</a>
+				</li>
+				<li className="nav-link">
+					<a href="#contact">Contact</a>
+				</li>
+			</ul>
+		</nav>
+	);
+}
