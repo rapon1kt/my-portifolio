0ecd4f5d7a6047c3c0e43f006d2fb3abfbca421c
rapon1kt <shinnatensei@gmail.com>
2024-03-07 21:38:22 -0300

    feat: export default components

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Divider } from "./divider/divider";
+export { default as Navbar } from "./navbar/navbar";
+export { default as Preloader } from "./preloader/preloader";
+export { default as Welcome } from "./welcome/welcome";
