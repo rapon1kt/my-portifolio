a8ed0be1e73542c6cb1db7fd033be6ce2d5a272b
rapon1kt <76401730+rapon1kt@users.noreply.github.com>
2024-10-07 02:26:16 +0000

    feat: add about-me component to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Navbar, Welcome } from "@/components";
+import { Navbar, Welcome, AboutMe } from "@/components";
 
 export default function Home() {
 	return (
 		<div id="container">
 			<Navbar />
 			<Welcome />
+			<AboutMe />
 		</div>
 	);
 }
