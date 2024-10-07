a9220cc162c040b8a4e4e25ddb909c21ec57087e
rapon1kt <76401730+rapon1kt@users.noreply.github.com>
2024-10-07 02:25:46 +0000

    feat: finished about-me component

--- src/components/about-me/about-me.tsx
+++ src/components/about-me/about-me.tsx
@@ -1,9 +1,59 @@
 import React from "react";
+import "./about-me.css";
+import { Typography } from "@mui/joy";
+import { Hash, Heart } from "lucide-react";
 
 export default function AboutMe() {
-    return(
-        <section id="about-me">
-            <h1>About Me</h1>
-        </section>
-    )
+  return (
+    <section id="about-me">
+      <div className="left-corner">
+        <Typography
+          className="about-me-title"
+          startDecorator={<Hash strokeWidth={3} size={30} />}
+        >
+          About Me
+        </Typography>
+        <Typography className="my-description">
+          Hi everyone, i am
+          <Typography className="colored"> Gustavo Raponi Silva </Typography>
+          from
+          <Typography className="colored"> Itajubá, Brasil</Typography>. I am
+          currently attending high school and intend to enter the Federal
+          University of Itajubá (UNIFEI)
+          <br /> <br />
+          Apart from coding, some other activities that I love to do!
+          
+        </Typography>
+        <Typography className="list">
+            <Typography
+              startDecorator={<Heart className="heart" />}
+              className="hobbies"
+            >
+              Voleyball
+            </Typography>
+            <Typography
+              startDecorator={<Heart className="heart" />}
+              className="hobbies"
+            >
+              Playing Games
+            </Typography>
+            <Typography
+              startDecorator={<Heart className="heart" />}
+              className="hobbies"
+            >
+              Study Philosophy
+            </Typography>
+          </Typography>
+          <Typography className="quote">
+            <i>
+              "Everything is empty, everything is the same and everything was!"
+            </i>
+            <Typography className="quote">-Friedrich, Nietzsche</Typography>
+          </Typography>
+      </div>
+      <div className="right-corner">
+        <img src="programming.png" alt="programming me" />
+      </div>
+    </section>
+  );
 }
