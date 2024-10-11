a6a7468b34c2f7c49c28a42d58063070be5c447f
rapon1kt <76401730+rapon1kt@users.noreply.github.com>
2024-10-11 14:27:09 +0000

    feat: add more about-me

--- src/components/about-me/about-me.tsx
+++ src/components/about-me/about-me.tsx
@@ -19,37 +19,39 @@ export default function AboutMe() {
           from
           <Typography className="colored"> Itajubá, Brasil</Typography>. I am
           currently attending high school and intend to enter the Federal
-          University of Itajubá (UNIFEI)
+          University of Itajubá (UNIFEI). I'm a technology enthusiast with a
+          positive mindset and a wild desire to learn new things. Since
+          beginning my journey as a Front-End Student, I have continuously
+          worked to improve my skills and optimize my projects more and more.
           <br /> <br />
           Apart from coding, some other activities that I love to do!
-          
         </Typography>
         <Typography className="list">
-            <Typography
-              startDecorator={<Heart className="heart" />}
-              className="hobbies"
-            >
-              Voleyball
-            </Typography>
-            <Typography
-              startDecorator={<Heart className="heart" />}
-              className="hobbies"
-            >
-              Playing Games
-            </Typography>
-            <Typography
-              startDecorator={<Heart className="heart" />}
-              className="hobbies"
-            >
-              Study Philosophy
-            </Typography>
+          <Typography
+            startDecorator={<Heart className="heart" />}
+            className="hobbies"
+          >
+            Voleyball
           </Typography>
-          <Typography className="quote">
-            <i>
-              "Everything is empty, everything is the same and everything was!"
-            </i>
-            <Typography className="quote">-Friedrich, Nietzsche</Typography>
+          <Typography
+            startDecorator={<Heart className="heart" />}
+            className="hobbies"
+          >
+            Playing Games
           </Typography>
+          <Typography
+            startDecorator={<Heart className="heart" />}
+            className="hobbies"
+          >
+            Study Philosophy
+          </Typography>
+        </Typography>
+        <Typography className="quote">
+          <i>
+            "Everything is empty, everything is the same and everything was!"
+          </i>
+          <Typography className="quote">-Friedrich, Nietzsche</Typography>
+        </Typography>
       </div>
       <div className="right-corner">
         <img src="programming.png" alt="programming me" />
