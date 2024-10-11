24f7eb9df34b58440c1dd4b3a8db29fe2744ed4e
rapon1kt <76401730+rapon1kt@users.noreply.github.com>
2024-10-11 13:46:39 +0000

    refactor: new style to menu-navbar and fix links

--- src/components/navbar/navbar-menu/navbar-menu.tsx
+++ src/components/navbar/navbar-menu/navbar-menu.tsx
@@ -1,81 +1,61 @@
 import * as React from "react";
-import IconButton from "@mui/material/IconButton";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
+import { MenuButton, Menu, Dropdown, MenuItem } from "@mui/joy";
 import { MenuIcon } from "lucide-react";
 
 const menuData = [
-	{
-		title: "About Me",
-		link: "#about-me",
-	},
-	{
-		title: "Skills",
-		link: "#skills",
-	},
-	{
-		title: "Projects",
-		link: "#projects",
-	},
-	{
-		title: "Contact",
-		link: "#contact",
-	},
+  {
+    title: "About Me",
+    link: "#about-me",
+  },
+  {
+    title: "Skills",
+    link: "#skills",
+  },
+  {
+    title: "Projects",
+    link: "#projects",
+  },
+  {
+    title: "Contact",
+    link: "#contact",
+  },
 ];
 
 function NavbarMenu() {
-	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-	const open = Boolean(anchorEl);
-	const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-		setAnchorEl(event.currentTarget);
-	};
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
-
-	return (
-		<div>
-			<IconButton
-				id="navbar-button"
-				aria-controls={open ? "navbar-menu" : undefined}
-				aria-haspopup="true"
-				aria-expanded={open ? "true" : undefined}
-				onClick={handleClick}
-			>
-				<MenuIcon color="#9675a5" />
-			</IconButton>
-			<Menu
-				id="navbar-menu"
-				aria-labelledby="navbar-button"
-				anchorEl={anchorEl}
-				open={open}
-				onClose={handleClose}
-				sx={{
-					borderRadius: "12px",
-				}}
-				MenuListProps={{
-					sx: {
-						backgroundColor: "#161218",
-						color: "#beb4c7",
-					},
-				}}
-				anchorOrigin={{
-					vertical: "top",
-					horizontal: "left",
-				}}
-				transformOrigin={{
-					vertical: "top",
-					horizontal: "left",
-				}}
-			>
-				{menuData.map((data) => (
-					<MenuItem LinkComponent={"a"} href={data.link}>
-						{data.title}
-					</MenuItem>
-				))}
-			</Menu>
-		</div>
-	);
+  return (
+    <Dropdown>
+      <MenuButton
+        sx={{
+          transition: "all ease-in 0.2s",
+          border: "none",
+          ":hover": {
+            backgroundColor: "#161218",
+            borderRadius: "50%",
+            padding: "1rem",
+          },
+        }}
+      >
+        <MenuIcon color="#9675a5" />
+      </MenuButton>
+      <Menu sx={{ backgroundColor: "#161218", borderColor: "#9675a5" }}>
+        {menuData.map((menuItem) => (
+          <MenuItem
+            sx={{
+              fontFamily: "Dosis",
+			  fontWeight: 600,
+              color: "#f1eff3",
+              ":hover": { backgroundColor: "#161218" },
+            }}
+            component={"a"}
+            key={menuItem.title}
+            href={menuItem.link}
+          >
+            {menuItem.title}
+          </MenuItem>
+        ))}
+      </Menu>
+    </Dropdown>
+  );
 }
 
 export default NavbarMenu;
